7068cd36742743eb1bd60f36a9fef2a7e41dec65
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-12-21 23:14:34 +0100

    Registration view

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -4,6 +4,9 @@
       <router-link v-bind:to="{name: 'home'}" class="brand">Microblog</router-link>
       <input id="cmenug" type="checkbox" class="show">
       <label for="cmenug" class="burger pseudo button">&#8801;</label>
+      <div class="menu">
+        <router-link v-bind:to="{name: 'register'}">Register</router-link>
+      </div>
     </nav>
     <div>
       <router-view/>
---
+++ ui/src/views/Register.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Register as na author on Microblog</h1>
+    <p>Enter your name below and start publishing!</p>
+    <form>
+      <div>
+        <label for="input-name">Name</label>
+        <input type="text" id="input-name" v-bind="authorName" value="">
+        <button type="submit" class="primary" @click="registerAuthor">Register</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Register',
+  data() {
+    return {
+      authorName: '',
+    };
+  },
+  methods: {
+    registerAuthor(e) {
+      e.preventDefault();
+    },
+  },
+};
+</script>
