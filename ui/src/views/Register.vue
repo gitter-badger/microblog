65ea380add6f752339fa2a535ac6797b4180e295
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-12-22 21:05:38 +0100

    Register view

--- ui/src/views/Register.vue
+++ ui/src/views/Register.vue
@@ -5,7 +5,8 @@
     <form>
       <div>
         <label for="input-name">Name</label>
-        <input type="text" id="input-name" v-bind="authorName" value="">
+        <input type="text" id="input-name" v-model="authorName" @change="checkNameAvailable">
+        <p id="name-check-result"></p>
         <button type="submit" class="primary" @click="registerAuthor">Register</button>
       </div>
     </form>
@@ -14,6 +15,8 @@
 
 
 <script>
+import slugify from 'slugify';
+
 export default {
   name: 'Register',
   data() {
@@ -22,9 +25,39 @@ export default {
     };
   },
   methods: {
-    registerAuthor(e) {
+    async registerAuthor(e) {
       e.preventDefault();
+      const url = '/api/authors';
+      const data = { name: this.authorName };
+      const resp = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      if (resp.status === 201) {
+        console.log('author created');
+      }
     },
+    async checkNameAvailable() {
+      const slug = slugify(this.authorName.toLowerCase());
+      const url = `/api/author/${slug}`;
+      const resp = await fetch(url);
+      const $el = document.getElementById('name-check-result');
+      if (resp.status !== 404) {
+        $el.innerText = 'This name is already taken';
+        $el.style.color = 'red';
+        $el.hidden = false;
+      } else {
+        $el.innerText = '';
+        $el.hidden = true;
+      }
+    },
+  },
+  mounted() {
+    const $el = document.getElementById('name-check-result');
+    $el.hidden = true;
   },
 };
 </script>
